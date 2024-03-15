f2f81fe2b5e608ac9fe0f436ccf983ffd17f13bf
sadaqatbarkat <153889463+sadaqatbarkat@users.noreply.github.com>
2024-03-15 18:31:28 +0500

    add index and app.js files

---
+++ app.js
@@ -0,0 +1,39 @@
+//Q1
+// let firstName;
+// let lastName;
+// let fullName;
+
+
+//Q2
+//P-1
+// let abc;
+// let def;
+// let fg12;
+// let good;
+// let one;
+
+
+//P-2
+
+// let $dollar;
+// let totalPrice;
+// let _marks;
+// let percentage;
+// let fatherName;
+
+//Q3
+//a
+//   _  $  name  _123 ;
+
+//b _ $ name;
+
+
+//c
+// case sensitive
+
+
+//d
+//keywords
+
+
+
